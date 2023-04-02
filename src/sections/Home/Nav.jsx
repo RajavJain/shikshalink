cdc14e0077ecb6119064e461ccfbbdb5c09b0b47
utsav82 <utsa.yadav.94@gmail.com>
2023-04-02 16:41:33 +0530

    Added different pages

---
+++ src/sections/Home/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+import { Link } from "react-router-dom";
+function Nav() {
+  return (
+    <nav className="header">
+     nav bar
+    </nav>
+  );
+}
+
+export default Nav;
