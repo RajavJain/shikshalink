5727a735b8f3289d1441c0f7a86eec1fe6f3ce79
utsav82 <utsa.yadav.94@gmail.com>
2023-04-03 02:26:41 +0530

    small tweaks to nav bar

--- src/sections/Home/Nav.jsx
+++ src/sections/Home/Nav.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Nav.css";
+import { useState, useEffect } from 'react';
 function Nav() {
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const isTop = window.scrollY > 0;
+      setScrolled(isTop);
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
   return (
-    <nav className="header">
+    <nav className={`header${scrolled ? ' scrolled' : ''}`}>
       <div className="header__left">
        <img src="https://18pixels.com/img/18pixels.png" alt="Shikshalink-logo" />
       </div>
