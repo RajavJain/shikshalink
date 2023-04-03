9e905252d7112aa29622fef49022e9abfddab991
utsav82 <utsa.yadav.94@gmail.com>
2023-04-04 02:56:11 +0530

    Setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-
 import "./App.css";
-import Nav from "./sections/Home/Nav"
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Nav from "./pages/Home/Nav";
+import Home from "./pages/Home/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+
 function App() {
-  
-  return <>
-  <Nav></Nav>
-  </>;
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <Nav></Nav>
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/About" element={<About></About>}></Route>
+            <Route path="/Contact" element={<Contact></Contact>}></Route>
+            <Route path="/Services" element={<Services></Services>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
